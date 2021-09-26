9a30b0a2f74c2bc4cdfd6d903b87c4073c31aec7
jordengerovac <jordengerovac@gmail.com>
2021-09-26 18:57:20 -0400

    cors configuration

--- src/main/java/com/bandmates/application/security/SecurityConfiguration.java
+++ src/main/java/com/bandmates/application/security/SecurityConfiguration.java
@@ -42,24 +42,30 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
         customAuthenticationFilter.setFilterProcessesUrl("/api/v1/login");
-        http.cors();
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests().antMatchers("/api/v1/login/**", "/api/v1/token/refresh/**").permitAll();
         http.authorizeRequests().antMatchers(OPTIONS, "/api/v1/**").permitAll();
-        http.authorizeRequests().antMatchers(POST, "/api/v1/users/create/**").permitAll();
+        http.authorizeRequests().antMatchers(POST, "/api/v1/users/create/**").hasAnyAuthority("ROLE_USER");;
         http.authorizeRequests().antMatchers(POST, "/api/v1/users/register/**").permitAll();
         http.authorizeRequests().antMatchers(GET, "/api/v1/users/confirm/**").permitAll();
+        http.authorizeRequests().antMatchers(POST, "/api/v1/botb/**").hasAnyAuthority("ROLE_USER");
+        http.authorizeRequests().antMatchers(PUT, "/api/v1/botb/**").hasAnyAuthority("ROLE_USER");
+        http.authorizeRequests().antMatchers(GET, "/api/v1/botb/**").hasAnyAuthority("ROLE_USER");;
+        http.authorizeRequests().antMatchers(DELETE, "/api/v1/botb/**").hasAnyAuthority("ROLE_USER");
         http.authorizeRequests().antMatchers(GET, "/api/v1/users/**").hasAnyAuthority("ROLE_USER");
+        http.authorizeRequests().antMatchers(POST, "/api/v1/users/**").hasAnyAuthority("ROLE_USER");
         http.authorizeRequests().antMatchers(PUT, "/api/v1/users/**").hasAnyAuthority("ROLE_USER");
         http.authorizeRequests().antMatchers(GET, "/api/v1/profiles/**").hasAnyAuthority("ROLE_USER");
         http.authorizeRequests().antMatchers(PUT, "/api/v1/profiles/**").hasAnyAuthority("ROLE_USER");
         http.authorizeRequests().antMatchers(POST, "/api/v1/profiles/**").hasAnyAuthority("ROLE_USER");
         http.authorizeRequests().antMatchers(POST, "/api/v1/spotifydata/**").hasAnyAuthority("ROLE_USER");
+        http.authorizeRequests().antMatchers(PUT, "/api/v1/spotifydata/**").hasAnyAuthority("ROLE_USER");
         http.authorizeRequests().antMatchers(GET, "/api/v1/spotifydata/**").hasAnyAuthority("ROLE_USER");
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(customAuthenticationFilter);
         http.addFilterBefore(new CustomAuthorizationFilter(userRepository), UsernamePasswordAuthenticationFilter.class);
+        http.cors();
     }
 
     @Bean
